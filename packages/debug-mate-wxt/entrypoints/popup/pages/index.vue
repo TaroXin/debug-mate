04bb9963a9092bed507320bee660d10701620f5a
TaroXin <15029352778@163.com>
2025-01-10 11:16:48 +0800

    fix(debug-mate-wxt): 移除未使用的导入

--- packages/debug-mate-wxt/entrypoints/popup/pages/index.vue
+++ packages/debug-mate-wxt/entrypoints/popup/pages/index.vue
@@ -2,13 +2,7 @@
 import type { NeedVariableWithValue } from '@debug-mate/types'
 import type { FormInst, FormRules } from 'naive-ui'
 import type { DatePickerType } from 'naive-ui/es/date-picker/src/config'
-import { useSystemStore } from '@/store/system.ts'
-import {
-  getOriginEnabled,
-  getVariableConfig,
-  setOriginEnabled,
-} from '@/utils/settings.ts'
-import { verifyEmail, verifyUrl } from '@/utils/verify.ts'
+import { useSystemStore } from '@/store/system'
 import { getConfigKey, getValueKey } from '@debug-mate/shared'
 
 const systemStore = useSystemStore()
